a5efb4ea7330443b713c0f2f9ec88a7bfa04b3f9
PADAKANTI HARSHITH <harshaharshith31@gmail.com>
2026-03-08 23:20:55 +0530

    Change datetime to IST for recent submissions
    Updated datetime handling to use IST for submissions.

--- send.py
+++ send.py
@@ -1,6 +1,6 @@
 import requests
 import os
-from datetime import datetime, timezone
+from datetime import datetime, timedelta
 
 username = os.environ["LEETCODE_USERNAME"]
 webhook = os.environ["DISCORD_WEBHOOK"]
@@ -22,14 +22,18 @@ query = {
 
 res = requests.post(url, json=query).json()
 
-today = datetime.now(timezone.utc).date()
+# current time in IST
+now = datetime.utcnow() + timedelta(hours=5, minutes=30)
+today = now.date()
 
 links = []
 
 for sub in res["data"]["recentSubmissionList"]:
     if sub["statusDisplay"] == "Accepted":
-        d = datetime.fromtimestamp(int(sub["timestamp"]), tz=timezone.utc).date()
-        if d == today:
+        ts = int(sub["timestamp"])
+        sub_time = datetime.utcfromtimestamp(ts) + timedelta(hours=5, minutes=30)
+
+        if sub_time.date() == today:
             links.append(f"https://leetcode.com/problems/{sub['titleSlug']}")
 
 links = list(dict.fromkeys(links))
